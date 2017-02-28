f7d3e1a81cd8b04f174f13c096e441edef29d57f
gwy15 <gwy15thu@gmail.com>
2017-02-28 20:52:23 +0800

    加入了时间打印的功能

--- ProgressBar.py
+++ ProgressBar.py
@@ -1,14 +1,17 @@
 import math
+import time
 class ProgressBar:
     global charset
     charset = "▏▎▍▌▋▊▉█"
 
-    def __init__(self, maxLength, maxCount, printCount=True, printPercentage=False):
+    def __init__(self, maxLength, maxCount, printCount=True, printPercentage=False, printTime=False):
         self.maxLength = maxLength
         self.maxCount = maxCount
         self.currentCount = 0
         self.printCount = printCount
         self.printPercentage = printPercentage
+        self.printTime = printTime
+        self.startTime = time.time()
 
     def update(self, currentCount, maxCount=None):
         self.currentCount = currentCount
@@ -30,6 +33,17 @@ class ProgressBar:
         string += '  ' * (self.maxLength - len(string)) # fill with blank
         string += charset[0] # print boarder
 
+        if self.printTime:
+            secondsSpent = math.floor(time.time() - self.startTime)
+            if secondsSpent < 3600:
+                timeStr = time.strftime('%M:%S', time.gmtime(secondsSpent))
+            elif secondsSpent < 3600*24:
+                timeStr = time.strftime('%H:M:%S', time.gmtime(secondsSpent))
+            else:
+                days = secondsSpent // (3600*24)
+                seconds = secondsSpent % (3600*24)
+                timeStr = int(days) + time.strftime('days %H:M:%S', time.gmtime(seconds))
+            string = timeStr + ' ' + string
         if self.printPercentage:
             string += "%2.1f%% "%(percentage*100.,)
         if self.printCount:
@@ -41,9 +55,9 @@ class ProgressBar:
         print(string, end='')
 
 if __name__ == '__main__':
-    bar = ProgressBar(50, 500, printPercentage=True)
+    bar = ProgressBar(50, 500, printPercentage=True, printTime=True)
     import time
     for i in range(501):
         bar.update(i)
-        time.sleep(0.05)
+        time.sleep(0.01)
     print()
