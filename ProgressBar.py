850d44d28200a968a2f6242ff858238c71f77be3
gwy15 <gwy15thu@gmail.com>
2017-03-16 16:40:24 +0800

    增加一个修饰器

--- ProgressBar.py
+++ ProgressBar.py
@@ -59,9 +59,26 @@ class ProgressBar:
     def __del__(self):
         print()
 
+def growBar(bar:ProgressBar, *args, **kw):
+    def decorator(func):
+        def wrapper(*args1, **kw1):
+            res = func(*args1, **kw1)
+            bar.grow(*args, **kw)
+        return wrapper
+    return decorator
+
 if __name__ == '__main__':
     bar = ProgressBar(500, 50, printPercentage=True, printTime=True)
     import time
     for i in range(501):
         bar.update(i)
         time.sleep(0.01)
+
+    del bar
+    bar = ProgressBar(500, 40)
+    @growBar(bar, 2)
+    def fun(a):
+        time.sleep(0.01)
+    
+    for i in range(250):
+        fun(2)
